f0a7afaf152a6aee14473f4843d2b184b03fc9d5
Jarett Young <know_jarett@yahoo.com>
2022-11-30 13:17:36 -0600

    added vscode-xammp

---
+++ install-vscode.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo curl https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64 -o ~/Downloads/vs-code.deb
+
+sudo apt install ~/Downloads/vs-code.deb
---
+++ install-xammp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo curl https://sourceforge.net/projects/xampp/files/XAMPP%20Linux/8.1.12/xampp-linux-x64-8.1.12-0-installer.run/download -o ~/Downloads/xammp-install.run
+
+cd ~/Downloads
+
+sudo chmod +x xammp-install.run
+
+sudo ./xammp-install.run
